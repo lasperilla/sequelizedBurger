b0d73eb839de25e1f3a5cb0781575bfcb0edd32b
lasperilla <l.asperilla@gmail.com>
2017-05-23 10:48:10 -0400

    edited index.js, server.js, burger model

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 8080;
-
+// Sets up the Express App
+// =============================================================
 var app = express();
+var PORT = process.env.PORT || 8080;
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+// Requiring our models for syncing
+var db = require("./models");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("./public"));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
@@ -20,9 +28,14 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+// Routes =============================================================
 
-app.use("/", routes);
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-app.listen(port);
+// Syncing our sequelize models and then starting our express app
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
